9ab7f4b53e57f662a4366813993850beb2c1ce79
snwmelt <ekitai.k@gmail.com>
2018-04-17 18:53:27 +0100

    Added Initial Login Page View
    - Moved MainWindow to MainWindowView
    - Moved MainWindowView to correct namespace
    - Added MainWindowViewModel, LoginPageViewModel and LoginPageView
    - Added Frame to MainWindowView xaml to allow dynamic Page/Content loading utalizing MainWindowViewModel

---
+++ PennyDreadless/View Models/LoginPageViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.ComponentModel;
+using Walkways.MVVM.View_Model;
+
+namespace PennyDreadless.View_Models
+{
+    internal class LoginPageViewModel : INotifyPropertyChanged
+    {
+        #region Private Variables
+
+        private String      _Password;
+        private String      _Username;
+        private INPCInvoke  _INPCInvoke;
+
+        #endregion
+
+        public LoginPageViewModel ( )
+        {
+            _INPCInvoke         = new INPCInvoke( this );
+            CreateUserCommand   = new CommandRelay<Object>( CreateUser, InputIsValid );
+            ValidateUserCommand = new CommandRelay<Object>( ValidateUser, InputIsValid );
+
+            Password = "Password";
+            Username = "Username";
+        }
+
+        private bool InputIsValid( Object obj )
+        {
+            return !( obj is null ); // Need to write proper validation
+        }
+
+        public void CreateUser( Object obj )
+        {
+            // Implement Database Create User?
+        }
+
+        public CommandRelay<Object> CreateUserCommand
+        {
+            get;
+        }
+        
+
+        public String Password
+        {
+            private get
+            {
+                return _Password;
+            }
+
+            set
+            {
+                _INPCInvoke.AssignPropertyValue<String>( ref PropertyChanged, ref _Password, value );
+            }
+        }
+
+        public String Username
+        {
+            private get
+            {
+                return _Username;
+            }
+
+            set
+            {
+                _INPCInvoke.AssignPropertyValue<String>( ref PropertyChanged, ref _Username, value );
+            }
+        }
+
+        public void ValidateUser( Object obj )
+        {
+            // Implement Database Validate User?
+        }
+
+        public CommandRelay<object> ValidateUserCommand
+        {
+            get;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ PennyDreadless/View Models/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using PennyDreadless.Views;
+using System.ComponentModel;
+using System.Windows;
+using Walkways.MVVM.View_Model;
+
+namespace PennyDreadless.View_Models
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region Private Variables
+
+        private FrameworkElement _CurrentView;
+        private INPCInvoke       _INPCInvoke;
+
+        #endregion
+
+        public MainWindowViewModel( )
+        {
+            _INPCInvoke = new INPCInvoke( this );
+            CurrentView = new LoginPageView( );
+        }
+
+        public FrameworkElement CurrentView
+        {
+            get
+            {
+                return _CurrentView;
+            }
+
+            set
+            {
+                _INPCInvoke.AssignPropertyValue<FrameworkElement>( ref PropertyChanged, ref _CurrentView, value );
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
